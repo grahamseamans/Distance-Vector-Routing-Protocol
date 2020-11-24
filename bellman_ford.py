19fcf0d1a700532cec4bdf5d9ca79ef72c666c73
graham <seamansgraham@gmail.com>
2020-11-24 12:44:13 -0800

    all done

--- bellman_ford.py
+++ bellman_ford.py
@@ -20,7 +20,7 @@ class node:
         while self.update_queue:
             (sender, senders_routing_table) = self.update_queue.pop(0)
             self.update(sender, senders_routing_table)
-            self.update_routing()
+        self.update_routing()
 
     def broadcast(self):
         for n in self.neighbors:
@@ -45,7 +45,6 @@ class node:
         trans = np.transpose(self.distance_table)
         for i, row in enumerate(trans):
             fastest = min(row)
-            print('my id', self.id, row, fastest, self.routing_table[i])
             if fastest < self.routing_table[i]:
                 self.updated = True
                 self.routing_table[i] = fastest
@@ -55,33 +54,42 @@ class node:
         print(np.transpose(self.distance_table))
         print('routing table', self.routing_table)
 
+def pos_to_char(pos):
+    return chr(pos + 97)
+
 if __name__ == "__main__":
-    network_size = 4
+    network_size = 8
+    t = {pos_to_char(a): a for a in range(10)}
+
     # for i in range(network_size):
         # def __init__(self, id, neighbors, neighbor_distance, network_size) -> None:
-    edges.append(node(0, [1,2], [3,23], network_size))
-    edges.append(node(1, [0,2], [3,2], network_size))
-    edges.append(node(2, [0,1,3], [23,2,5], network_size))
-    edges.append(node(3, [2], [5], network_size))
+    # [(0, 'a'), (1, 'b'), (2, 'c'), (3, 'd'), (4, 'e'), (5, 'f'), (6, 'g'), (7, 'h'), (8, 'i'), (9, 'j')]
+
+
+    edges.append(node(0, [1, 2], [14,2], network_size))
+    edges.append(node(1, [0,2,5,4], [14,8,3,1], network_size))
+    edges.append(node(2, [0,1,6], [2,8,1], network_size))
+    edges.append(node(3, [4,5], [7,10], network_size))
+    edges.append(node(4, [1,3,5], [1,7,1], network_size))
+    edges.append(node(5, [1,3,4,6,7], [3,10,1,3,1], network_size))
+    edges.append(node(6, [2,5,7], [1,3,5], network_size))
+    edges.append(node(7, [5,6], [1,5], network_size))
 
     for e in edges:
         e.print_node()
 
     print('\nINITED\n')
 
-    edges[0].update_routing()
-
-    print('\nINITED\n')
-
-
+    # for i in range(5):
     while edges[0].updated:
-        # for i in range(16):
+        print(edges[0].routing_table)
         for e in edges:
             e.turn()
     '''
     for e in edges:
         e.turn()
     '''
+    print('\nDONE\n')
     for e in edges:
         e.print_node()
 
